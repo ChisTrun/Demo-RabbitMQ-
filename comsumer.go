b1f63d11c6ffe2ddfaa2436c1ec0a9abd8846280
Your Name <you@example.com>
2025-04-14 14:36:46 +0700

    init

---
+++ comsumer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://skillsharp:Skillsharp@123@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("my-queue", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs, err := ch.Consume(q.Name, "", false, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("📥 Waiting for messages...")
+
+	for msg := range msgs {
+		log.Printf("📨 Received: %s", msg.Body)
+		// ✅ Acknowledge = xoá message khỏi queue
+		// msg.Ack(false)
+	}
+}
---
+++ producer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://skillsharp:Skillsharp@123@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("my-queue", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := "Hello RabbitMQ!"
+	err = ch.Publish("", q.Name, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("📤 Sent:", body)
+}
